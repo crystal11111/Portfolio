7ad8687a22b7c7bb08864b98726922f9194559e9
unknown <crystal031230@gmail.com>
2023-07-05 06:39:34 -0600

    crystal

---
+++ components/Cards.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import Carditem from './Carditem'
+import './Cards.css'
+
+function Cards() {
+  return (
+    <div className='cards'>
+        <h1> Check out my resume!</h1>
+        <div className='cards__container'>
+            <div className='cards__wrapper'>
+                <ul className='cards_item'>
+                    <Carditem 
+                    src='/img-2.jpg'
+                    text='Check out my extracurricular activites'
+                    label='Activity'
+                    path='/services'
+                    />
+                    <Carditem
+                    src='img-2.jpg'
+                    text='Check out my projects'
+                    label='Projects'
+                    path='/services'
+                    />
+                </ul>
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Cards
---
+++ components/pages/Coffee.js
@@ -0,0 +1,6 @@
+import React from "react";
+import '../../App.css';
+
+export default function Coffee() {
+    return <h1 className="coffee">COFFEE</h1>;
+}
